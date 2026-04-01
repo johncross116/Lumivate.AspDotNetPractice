92276b7ae463294abb3d68870fd72b654af42caf
John <johncross116@gmail.com>
2026-03-31 21:46:45 -0400

    comments for interface files

--- Lumivate.TurtleStore/Lumivate.TurtleStore/Services/ICartService.cs
+++ Lumivate.TurtleStore/Lumivate.TurtleStore/Services/ICartService.cs
@@ -2,11 +2,11 @@ using Lumivate.TurtleStore.Models;
 
 namespace Lumivate.TurtleStore.Services
 {
-    // TODO-checkpoint-4: Create the ICartService interface
-    // Define the following method signatures:
-    //   - List<CartItem> GetCartItems()
-    //   - void AddToCart(int turtleId)
-    //   - void RemoveFromCart(int turtleId)
-    //   - decimal GetCartTotal()
-    //   - void ClearCart()
+	// TODO-checkpoint-4: Create the ICartService interface
+	// Define the following method signatures. Make sure these signatures match the methods you will implement in CartService.cs.
+	//   - List<CartItem> GetCartItems()
+	//   - void AddToCart(int turtleId)
+	//   - void RemoveFromCart(int turtleId)
+	//   - decimal GetCartTotal()
+	//   - void ClearCart()
 }
